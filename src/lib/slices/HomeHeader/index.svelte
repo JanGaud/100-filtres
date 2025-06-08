001fb3a5f62bb9f552527e307ced2fd80323e8d6
JanGaud <JanisGaudreault.jg@gmail.com>
2025-06-07 21:52:36 -0400

    start of header

--- src/lib/slices/HomeHeader/index.svelte
+++ src/lib/slices/HomeHeader/index.svelte
@@ -5,9 +5,17 @@
 	export let slice: Content.HomeHeaderSlice;
 </script>
 
-<section class="relative mt-30 lg:mt-0 h-[650px] lg:h-screen overflow-x-hidden" data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+<section
+	class="relative mt-30 lg:mt-0 h-[650px] lg:h-screen overflow-x-hidden"
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+>
 	<div class="absolute lg:left-30 bottom-0 flex items-center justify-center w-[250px] z-0">
-		<img class="w-full h-full object-cover" src="./static/img/hand-painted-neutral-coloured-watercolour-splatter-design.png" alt="coffee stain" />
+		<img
+			class="w-full h-full object-cover"
+			src="/img/hand-painted-neutral-coloured-watercolour-splatter-design.png"
+			alt="coffee stain"
+		/>
 	</div>
 
 	<div class="mx-auto overflow-hidden h-[500px] w-full rounded-4xl shadow-lg relative z-10">
